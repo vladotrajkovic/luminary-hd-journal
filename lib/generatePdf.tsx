2c2d92970da035522d16c4a2ce0e4f19ed313e72
vladotrajkovic <65129723+vladotrajkovic@users.noreply.github.com>
2026-03-14 12:31:16 +0100

    Add files via upload

--- lib/generatePdf.tsx
+++ lib/generatePdf.tsx
@@ -227,11 +227,11 @@ export async function generateHDReportPdf(data: ReportData): Promise<void> {
       paddingLeft: 4,
     },
     bulletDot: {
-      fontSize: 11,
-      fontFamily: 'Times-Roman',
-      color: C.gold,
+      width: 5,
+      height: 5,
+      backgroundColor: C.gold,
       marginRight: 9,
-      width: 10,
+      marginTop: 4,
     },
     bulletText: {
       flex: 1,
@@ -476,7 +476,7 @@ export async function generateHDReportPdf(data: ReportData): Promise<void> {
         if (next && next.type === 'bullet') {
           nextEl = React.createElement(
             View, { style: S.bulletRow },
-            React.createElement(Text, { style: S.bulletDot }, '\u25C6'),
+            React.createElement(View, { style: S.bulletDot }),
             React.createElement(Text, { style: S.bulletText }, ...inlineNodes(next.segs, Text))
           )
         } else if (next && next.type === 'paragraph') {
@@ -509,7 +509,7 @@ export async function generateHDReportPdf(data: ReportData): Promise<void> {
       if (b.type === 'bullet') {
         elements.push(React.createElement(
           View, { key: i, style: S.bulletRow },
-          React.createElement(Text, { style: S.bulletDot }, '\u25C6'),
+          React.createElement(View, { style: S.bulletDot }),
           React.createElement(Text, { style: S.bulletText }, ...inlineNodes(b.segs, Text))
         ))
         i++
